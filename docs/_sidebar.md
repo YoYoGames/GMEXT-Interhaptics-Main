4ed28cb1b3115a46a45fd52ebf3731379a56b3dc
Francisco Dias <fdias@opera.com>
2024-04-19 10:09:13 +0100

    Updated the documentation (first revision)

---
+++ docs/_sidebar.md
@@ -0,0 +1,7 @@
+## [Interhaptics Extension](home)
+
+### Modules
+* ${module.engine_control}
+* ${module.engine_events}
+* ${module.providers}
+* ${module.shared_types}
--- docs/documentation.js
+++ docs/documentation.js
@@ -1,175 +1,391 @@
+/*
+INTERHAPTICS DOCS: www.interhaptics.com/doc/interhaptics-engine/
+*/
 
+// Functions
 
 /**
  * @func interhaptics_init
- * @desc 
- * @returns {real}
+ * @desc This function initialises the different components and modules of the Interhaptics Engine:
+ * * Haptic Material Manager: module in charge of loading and storing haptic effects
+ * * Human Avatar Manager: module in charge of mapping between device, human avatar, and experience
+ * * Haptic Event Manager: module in charge of the control of haptic sources
+ * @returns {boolean}
+ * @func_end
  */
 function interhaptics_init() {}
 
 
 /**
  * @func interhaptics_quit
- * @desc 
- * @returns {real}
+ * @desc This function cleans up the different components and modules of the Interhaptics Engine. 
+ * To be called before the application is quit.
+ * @func_end
  */
 function interhaptics_quit() {}
 
 
 /**
  * @func interhaptics_add_hm
- * @desc 
- * @param {string} content
+ * @desc This function adds the content of an .haps file to the Interhaptics Engine for future use. It returns a reference to the new haptics file contents to be used in other functions.
+ * @param {string} content The JSON content of the .haps file to be loaded. This needs to follow the Interhaptics .haps format.
  * @returns {real}
+ * @func_end
  */
 function interhaptics_add_hm(content) {}
 
 
 /**
  * @func interhaptics_update_hm
- * @desc 
- * @param {real} material_ref
- * @param {string} content
- * @returns {real}
+ * @desc This function replaces the content of an already loaded haptic effect and returns whether the replace was successful.
+ * This can be useful when the ID of the haptic effect needs to be persistent. 
+ * @param {real} material_ref ID of the haptic effect to be replaced.
+ * @param {string} content The JSON content of the .haps file to be loaded. This needs to follow the Interhaptics .haps format.
+ * @returns {boolean}
+ * @func_end
  */
 function interhaptics_update_hm(material_ref, content) {}
 
 
 /**
  * @func interhaptics_play_event
- * @desc 
- * @param {real} material_ref
- * @param {real} vibration_offset
- * @param {real} texture_offset
- * @param {real} stiffness_offset
- * @returns {real}
+ * @desc This function starts the rendering playback of a haptic source. 
+ * Sets the starting time to 0 + different offsets. 
+ * If the event is already playing, it restarts with the new offets.
+ * If the source does not already exist, it will be created.
+ * @param {real} material_ref The ID of the source to play, which is the same as the attached haptic effect.
+ * @param {real} vibration_offset The vibration offset for the source.
+ * @param {real} texture_offset The texture offset for the source.
+ * @param {real} stiffness_offset The stiffness offset for the source.
+ * @func_end
  */
 function interhaptics_play_event(material_ref, vibration_offset, texture_offset, stiffness_offset) {}
 
 
 /**
  * @func interhaptics_stop_event
- * @desc 
- * @param {real} material_ref
- * @returns {real}
+ * @desc This function stops the rendering playback of a haptic source.
+ * @param {real} material_ref The ID of the source to stop, which is the same as the attached haptic effect.
+ * @func_end
  */
 function interhaptics_stop_event(material_ref) {}
 
+/**
+ * @func interhaptics_add_target_to_event
+ * @desc This function adds a target within the range of the source. The Interhaptics Engine will remap device endpoints and in-range targets to the device management layer for haptic playback.
+ * @param {real} material_ref The ID of the source to add a target to, which is the same as the attached haptic effect.
+ * @param {array[struct.Interhaptics_CommandData]} target A target contains a group of body parts, lateral flags, and exclusion flags.
+ * @func_end
+ */
+function interhaptics_add_target_to_event(material_ref, target) {}
+
 
 /**
- * @func interhaptics_add_target_to_event_multiplatform
- * @desc 
- * @param {real} material_ref
- * @param {Id.Buffer} buff_args
- * @returns {real}
+ * @func interhaptics_remove_target_from_event
+ * @desc This function adds a target within the range of the source. The Interhaptics Engine will remap device endpoints and in-range targets to the device management layer for haptic playback.
+ * @param {real} material_ref The ID of the source to add a target to, which is the same as the attached haptic effect.
+ * @param {array[struct.Interhaptics_CommandData]} target An array of ${struct.Interhaptics_CommandData} to build a target. A target contains a group of body parts, lateral flags, and exclusion flags. Only perfectly matching targets will be removed.
+ * @func_end
  */
-function interhaptics_add_target_to_event_multiplatform(material_ref, buff_args) {} // FIXME 
-
-
-/**
- * @func interhaptics_remove_target_from_event_multiplatform
- * @desc 
- * @param {real} material_ref
- * @param {Id.Buffer} buff_args
- * @returns {real}
- */
-function interhaptics_remove_target_from_event_multiplatform(material_ref, buff_args) {} // FIXME 
+function interhaptics_remove_target_from_event(material_ref, target) {}
 
 
 /**
  * @func interhaptics_remove_all_targets_from_event
- * @desc 
- * @param {real} material_ref
- * @returns {real}
+ * @desc This function removes all targets from a source range. The Interhaptics Engine will remap device endpoints and in-range targets to the device management layer for haptic playback.
+ * @param {real} material_ref The ID of the source to remove all targets from, which is the same as the attached haptic effect.
+ * @func_end
  */
 function interhaptics_remove_all_targets_from_event(material_ref) {}
 
 
 /**
  * @func interhaptics_compute_all_events
- * @desc 
- * @param {real} current_time
- * @returns {real}
+ * @desc This function must be called in the application main loop to trigger the rendering of all haptic buffers at a specific time. The Interhaptics Engine will compare the current time with the last known value to build a buffer large enough to cover frame drops. This function can be called from the main thread or in a parallel loop. It must be called at least once before triggering the device update event.
+ * @param {real} current_time The current time in seconds.
+ * @func_end
  */
 function interhaptics_compute_all_events(current_time) {}
 
 
 /**
- * @func interhaptics_update_event_positions_multiplatform
- * @desc 
- * @param {real} material_ref
- * @param {Id.Buffer} buff_args
- * @param {real} texture_position
- * @param {real} stiffness_position
- * @returns {real}
+ * @func interhaptics_update_event_positions
+ * @desc This function updates the spatial positions for a specific source target.
+ * @param {real} material_ref The ID of the source, which is the same as the attached haptic effect.
+ * @param {array[struct.Interhaptics_CommandData]} target A vector of ${struct.Interhaptics_CommandData} to build a target. A target contains a group of body parts, lateral flags, and exclusion flags. Only perfectly matching targets will be updated.
+ * @param {real} texture_position The new texture position.
+ * @param {real} stiffness_position The new stiffness position.
+ * @func_end
  */
-function interhaptics_update_event_positions_multiplatform(material_ref, buff_args, texture_position, stiffness_position) {} // FIXME 
+function interhaptics_update_event_positions(material_ref, target, texture_position, stiffness_position) {}
 
 
 /**
  * @func interhaptics_set_event_offsets
- * @desc 
- * @param {real} material_ref
- * @param {real} vibration_offset
- * @param {real} texture_offset
- * @param {real} stiffness_offet
- * @returns {real}
+ * @desc This function sets the offsets for a specific haptic source.
+ * @param {real} material_ref The ID of the source, which is the same as the attached haptic effect.
+ * @param {real} vibration_offset The vibration offset.
+ * @param {real} texture_offset The texture offset.
+ * @param {real} stiffness_offet The stiffness offset.
+ * @func_end
  */
 function interhaptics_set_event_offsets(material_ref, vibration_offset, texture_offset, stiffness_offet) {}
 
 
 /**
  * @func interhaptics_clear_inactive_events
- * @desc 
- * @returns {real}
+ * @desc This function clears all inactive sources from memory. Inactive sources are kept in memory to avoid deletion and creation when playing and stopping a source.
+ * @func_end
  */
 function interhaptics_clear_inactive_events() {}
 
 
 /**
  * @func interhaptics_clear_active_events
- * @desc 
- * @returns {real}
+ * @desc This function clears all active sources from memory. Deleted sources can be recreated by calling the ${function.interhaptics_play_event} function.
+ * @func_end
  */
 function interhaptics_clear_active_events() {}
 
 
 /**
  * @func interhaptics_clear_event
- * @desc 
- * @param {real} material_ref
- * @returns {real}
+ * @desc This function clears a specific haptic source, whether it is active or not.
+ * @param {real} material_ref The ID of the source, which is the same as the attached haptic effect.
+ * @func_end
  */
 function interhaptics_clear_event(material_ref) {}
 
 
 /**
  * @func interhaptics_provider_init
- * @desc 
- * @returns {real}
+ * @desc This function handles the initialisation process of the specific device's COM, its haptic settings for rendering and subscription to the Interhaptics Engine. It returns `false` if the initialisation process fails.
+ * @returns {boolean}
+ * @func_end
  */
 function interhaptics_provider_init() {}
 
 
 /**
  * @func interhaptics_provider_is_present
- * @desc 
- * @returns {real}
+ * @desc This function checks the availability of the device before triggering haptic playback. It returns `true` if the provider is present, `false` if not.
+ * 
+ * This optional step can help improve performance.
+ * @returns {boolean}
+ * @func_end
  */
 function interhaptics_provider_is_present() {}
 
 
 /**
  * @func interhaptics_provider_provider_clean
- * @desc 
- * @returns {real}
+ * @desc This function is responsible for de-initialising the device's COM, if necessary, and for unsubscribing the provider from the Interhaptics Engine. It returns `false` if the de-initialisation process fails.
+ * @returns {boolean}
+ * @func_end
  */
 function interhaptics_provider_provider_clean() {}
 
 
 /**
  * @func interhaptics_provider_render_haptics
- * @desc 
- * @returns {real}
+ * @desc API triggers the rendering process for the provider by retrieving the necessary haptic buffers, transcoding them if required, and playing them back on the associated device. This API must be called for all targeted devices.
+ * It is mandatory to call ${function.interhaptics_compute_all_events} from the Interhaptics Engine before calling ${function.interhaptics_provider_render_haptics} for synchronized haptic rendering. Typically, both APIs are implemented in the same loop, with ${function.interhaptics_compute_all_events} called before the ${function.interhaptics_provider_render_haptics} calls.
+ * @func_end
  */
 function interhaptics_provider_render_haptics() {}
+
+// Constants
+
+/**
+ * @const Interhaptics_Operator
+ * @desc > **Interhaptics Constant** [Interhaptics_Operator](https://www.interhaptics.com/doc/interhaptics-engine/#operator)
+ *
+ * <br />
+ *
+ * This enum represents operators used in the haptic body mapping. The possible values are:
+ * @member Minus Represents a minus operator (-1)
+ * @member Neutral Represents a neutral operator (0)
+ * @member Plus Represents a plus operator (1)
+ * @const_end
+ */
+ 
+ 
+/**
+ * @const Interhaptics_LateralFlag
+ * @desc > **Interhaptics Constant** [Interhaptics_LateralFlag](https://www.interhaptics.com/doc/interhaptics-engine/#lateralflag)
+ *
+ * <br />
+ *
+ * This enum represents the lateral position of a body part. The possible values are:
+ * @member UnknownPosition Represents an unknown position. (-1)
+ * @member Global Represents the global position. (0)
+ * @member Right Represents the right position. (1)
+ * @member Left Represents the left position. (2)
+ * @member Center Represents the center position. (3)
+ * @const_end
+ */
+ 
+ /**
+ * @const Interhaptics_GroupID
+ * @desc > **Interhaptics Constant** [Interhaptics_GroupID](https://www.interhaptics.com/doc/interhaptics-engine/#groupid)
+ *
+ * <br />
+ *
+ * The GroupID enum represents the group IDs for different body parts. Each group has a unique ID assigned to it for grouping purposes. The following are some of the groups and their corresponding IDs
+ * @member Unknown Represents an unknown group. ID -1
+ * @member All Represents all body parts. ID 0
+ * @member Top Represents the top portion of the body. ID 100
+ * @member Down Represents the lower portion of the body. ID 101
+ * @member Arm Represents the arm group. ID 200
+ * @member Head Represents the head group. ID 201
+ * @member Chest Represents the chest group. ID 202
+ * @member Waist Represents the waist group. ID 203
+ * @member Leg Represents the leg group. ID 204
+ * @member Upper_arm Represents the upper arm group. ID 300
+ * @member Lower_arm Represents the lower arm group. ID 301
+ * @member Hand Represents the hand group. ID 302
+ * @member Skull Represents the skull group. ID 303
+ * @member Neck Represents the neck group. ID 304
+ * @member Upper_leg Represents the upper leg group. ID 305
+ * @member Lower_leg Represents the lower leg group. ID 306
+ * @member Foot Represents the foot group. ID 307
+ * @member Palm Represents the palm group. ID 400
+ * @member Finger Represents the finger group. ID 401
+ * @member Sole Represents the sole group. ID 402
+ * @member Toe Represents the toe group. ID 403
+ * @member Thumb Represents the thumb group. ID 500
+ * @member Index Represents the index finger group. ID 501
+ * @member Middle Represents the middle finger group. ID 502
+ * @member Ring Represents the ring finger group. ID 503
+ * @member Pinky Represents the pinky finger group. ID 504
+ * @member Hallux Represents the hallux group. ID 505
+ * @member Index_toe Represents the index toe group. ID 506
+ * @member Middle_toe Represents the middle toe group. ID 507
+ * @member Ring_toe Represents the ring toe group. ID 508
+ * @member Pinky_toe Represents the pinky toe group. ID 509
+ * @member First Represents the first segment group. ID 600
+ * @member Second Represents the second segment group. ID 601
+ * @member Third Represents the third segment group. ID 602
+ * @const_end
+ */
+ 
+ /**
+ * @struct Interhaptics_CommandData
+ * @desc > **Interhaptics Constant** [Interhaptics_CommandData](https://www.interhaptics.com/doc/interhaptics-engine/#commanddata)
+ *
+ * <br />
+ *
+ * This structure represents an instruction that is useful for finding the body part to render. It contains the following members:
+ * @member Sign {constant.Interhaptics_Operator} Represents the sign of the operation (+/-) of type Operator.
+ * @member Group {constant.Interhaptics_GroupID} Represents the group targeted of type ${constant.Interhaptics_GroupID}.
+ * @member Side {constant.Interhaptics_LateralFlag} Represents the side targeted of type ${constant.Interhaptics_LateralFlag}.
+ * @struct_end
+ */
+ 
+/**
+  * @module home
+  * @title Home
+  * @desc This is the Interhaptics extension for GameMaker, which allows you to add haptic feedback to your GameMaker games.
+  * 
+  * Interhaptics is a software that allows you to design, test, and play haptic effects. 
+  * 
+  * @section Modules
+  * @desc These are the modules of the Interhaptics extension. Each module links to the corresponding section of the Interhaptics documentation where possible.
+  * @ref module.engine_control
+  * @ref module.engine_events
+  * @ref module.providers
+  * @ref module.shared_types
+  * @section_end
+  * 
+  * @module_end
+  */
+
+/**
+  * @module engine_control
+  * @title Engine Control
+  * @desc **Interhaptics Docs Section:** [Engine Control](https://www.interhaptics.com/doc/interhaptics-engine/#engine-control)
+  * 
+  * <br />
+  * 
+  * This module contains the functions related to Engine Control.
+  * 
+  * @section_func
+  * @ref interhaptics_init
+  * @ref interhaptics_quit
+  * @ref interhaptics_add_hm
+  * @ref interhaptics_update_hm
+  * @section_end
+  * 
+  * @module_end
+  */
+
+
+/**
+  * @module engine_events
+  * @title Engine Events
+  * @desc **Interhaptics Docs Section:** [Engine Events](https://www.interhaptics.com/doc/interhaptics-engine/#engine-events)
+  * 
+  * <br />
+  * 
+  * This module contains the functions related to Engine Events.
+  * 
+  * @section_func
+  * @ref interhaptics_play_event
+  * @ref interhaptics_stop_event
+  * @ref interhaptics_add_target_to_event
+  * @ref interhaptics_remove_target_from_event
+  * @ref interhaptics_remove_all_targets_from_event
+  * @ref interhaptics_compute_all_events
+  * @ref interhaptics_update_event_positions
+  * @ref interhaptics_set_event_offsets
+  * @ref interhaptics_clear_inactive_events
+  * @ref interhaptics_clear_active_events
+  * @ref interhaptics_clear_event
+  * @section_end
+  * 
+  * @module_end
+  */
+
+
+/**
+  * @module providers
+  * @title Providers
+  * @desc **Interhaptics Docs Section:** [Key Concepts](https://www.interhaptics.com/doc/interhaptics-engine/#key-concepts)
+  * 
+  * <br />
+  * 
+  * This module contains the functions related to providers.
+  * 
+  * @section_func Functions
+  * @ref interhaptics_provider_init
+  * @ref interhaptics_provider_is_present
+  * @ref interhaptics_provider_provider_clean
+  * @ref interhaptics_provider_render_haptics
+  * @section_end
+  * 
+  * @module_end
+  */
+
+/**
+ * @module shared_types
+ * @title Shared Types
+ * @desc **Interhaptics Docs Section:** [Shared Types](https://www.interhaptics.com/doc/interhaptics-engine/#shared-types)
+ * 
+ * <br />
+ * 
+ * This module contains the constants and structs related to shared types.
+ * 
+ * @section_const Constants
+ * @desc The following are the constants that the Interhaptics extension defines. All of them are defined as an enum.
+ * @ref Interhaptics_Operator
+ * @ref Interhaptics_LateralFlag
+ * @ref Interhaptics_GroupID
+ * @section_end
+ *
+ * @section_struct Structs
+ * @desc The Interhaptics extension also defines a ${type.struct}: 
+ * @ref Interhaptics_CommandData
+ * @section_end
+ * 
+ * @module_end
+ */
